65d1d48848313a275f392a55eb12e1800ee272ea
Avery Berger <averyberger@icloud.com>
2024-10-05 16:41:26 -0500

    Did some shit, kinda broke it

--- calculations.js
+++ calculations.js
@@ -1,20 +1,48 @@
 class SpaceObjects {
-  constructor(VoX_, VoY_, x_, y_, mass_) {
+  constructor(VoX_,VoY_,x_,y_,mass_) {
     this.Vo = [VoX_, VoY_];
     this.pos = [x_, y_];
     this.mass = mass_;
-    this.r = CalculateDistance(this.pos);
+    this.r = 0;
   }
 }
 
 const x = 0;
 const y = 1;
 const squared = 2;
-const EarthsMass = 5.972;
-const GravitationalC = -6.674;
+const EarthsMass = 5.972 * (10**24);
+const GravitationalC = 6.6743 * (10**-11);
+const timeInt = 0.1;
 
-function CalculateDistance(pos) {
-  return Math.sqrt(pos[x] ** 2 + pos[y] ** 2);
+function CalculateDistance(spaceobj) {
+  
+
+  let coordinates = [];
+  for (let i = 0; i < 1000; i++){
+    coordinates.push([]);
+
+    
+    coordinates[i][x] = spaceobj.pos[x];
+    coordinates[i][y] = spaceobj.pos[y];
+    
+
+    spaceobj.r = (Math.sqrt(coordinates[i][x]**2 + coordinates[i][y]**2));
+    let a = (GravitationalC * EarthsMass) / (spaceobj.r**2);
+  
+    let angle = Math.atan(spaceobj.pos[x]/ spaceobj.pos[y]);
+    xAcceleration = Math.sin(angle) * a * Math.sign(spaceobj.pos[x]) *-1;
+    yAcceleration = Math.cos(angle) * a * Math.sign(spaceobj.pos[y]) *-1;
+  
+    deltaX = (xAcceleration * (timeInt**2) /2 + spaceobj.Vo[x] * timeInt);
+    deltaY = (yAcceleration * (timeInt**2) /2 + spaceobj.Vo[y] * timeInt);
+  
+    spaceobj.Vo[x] = ((spaceobj.Vo[x]**2) + 2 * xAcceleration * deltaX) **0.5 * -1;
+    spaceobj.Vo[y] = ((spaceobj.Vo[y]**2) + 2 * yAcceleration * deltaY) **0.5 * -1;
+    
+    spaceobj.pos[x] += deltaX;
+    spaceobj.pos[y] += deltaY;
+  }
+  console.log(coordinates);
 }
 
 //5.972 × 10^24
@@ -26,23 +54,27 @@ function Trajectory(spaceobj) {
     trajectory.push([]);
 
 
-    let accelaration = 1 / spaceobj.r ** squared;
-    let angle = Math.atan(spaceobj.pos[y]/spaceobj.pos[x]);
-    let xAcceleration = accelaration * Math.cos(angle)
-    let yAcceleration = accelaration *Math.sin(angle)
+    let accelaration = EarthsMass*GravitationalC / (spaceobj.r ** squared);
+
+    // console.log("a: " + a + " accelaration: " + accelaration);
+
+    let angle = Math.PI + Math.atan(spaceobj.pos[y]/spaceobj.pos[x]);
+    let xAcceleration = accelaration * Math.cos(angle);
+    let yAcceleration = accelaration * Math.sin(angle);
 
     //finds delta x,y
-    trajectory[i][x] = (spaceobj.Vo[x] + xAcceleration / squared);
-    trajectory[i][y] = (spaceobj.Vo[y] + yAcceleration / squared);
+    trajectory[i][x] = (spaceobj.Vo[x] + xAcceleration / 2);
+    trajectory[i][y] = (spaceobj.Vo[y] + yAcceleration / 2);
    
    
     spaceobj.r = CalculateDistance(trajectory[i]);
     spaceobj.pos[x] += trajectory[i][x];
     spaceobj.pos[y] += trajectory[i][y];
 
-    spaceobj.Vo[x] += xAcceleration; 
-    spaceobj.Vo[y] += yAcceleration;
+    spaceobj.Vo[x] += xAcceleration/2; 
+    spaceobj.Vo[y] += yAcceleration/2;
+    console.log(spaceobj.r);
+    console.log(accelaration);
   }
   return trajectory;
 }
-
